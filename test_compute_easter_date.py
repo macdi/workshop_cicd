922cb2f87655496da24be520c33a2d4b454395d5
mehdi <aichouch@gmail.com>
2019-10-25 16:15:45 +0200

    correct test program

--- test_compute_easter_date.py
+++ test_compute_easter_date.py
@@ -1,24 +1,24 @@
 import unittest
 
 
-from compute_easter_date import compute_easter_date 
+from compute_easter_date import compute_easter_date
 
 
 class TestComputeEasterDate(unittest.TestCase):
 
-	def test_compute(self):
-		annee_2006 = 2006
-        	result = compute_easter_date(annee_2006)
+    def test_compute(self):
+        annee_2006 = 2006
+        result = compute_easter_date(annee_2006)
 
-		self.assertEquals(4, result[0])
-		self.assertEquals(16, result[1] + 1)
+        self.assertEquals(4, result[0])
+        self.assertEquals(16, result[1] + 1)
 
-		annee_2030 = 2030
-		result = compute_easter_date(anne_2030)
-		self.assertEquals(4, result[0])
-		self.assertEquals(21, result[1])
+        annee_2030 = 2030
+        result = compute_easter_date(annee_2030)
 
+        self.assertEquals(4, result[0])
+        self.assertEquals(21, result[1] + 1)
 
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
